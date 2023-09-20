fae75c96f69c3108857409c2f5e674467169c28a
Besufekad-HAZ <besurye@gmail.com>
2023-09-20 18:42:00 +0300

    Add seeds and update Migration files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+greetings = [
+  "Welcome aboard! We're thrilled to have you join us.",
+  "Hi! How can we assist you today?",
+  "Greetings and salutations! Enjoy your time on our platform.",
+  "Hello, friend! Wishing you a wonderful experience here.",
+  "Good day! We're here to make your journey delightful."
+]
+
+greetings.each do |message|
+  Greeting.create!(message: message)
+end
